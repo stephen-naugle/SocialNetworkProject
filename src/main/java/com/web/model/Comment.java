665f4c7c50a630d19e04e1b68476d74343b5b6a4
ar-pearse <andrew.roy.pearse@gmail.com>
2020-11-02 18:23:15 -0600

    Created model layer and added some dependencies

---
+++ src/main/java/com/web/model/Comment.java
@@ -0,0 +1,72 @@
+package com.web.model;
+
+//TODO: Optional model, may not use
+
+/**
+ * @author Andrew Pearse Comment Model
+ */
+public class Comment {
+
+	private int commentId;
+	private int postId;
+	private String author;
+	private String comment;
+
+	/**
+	 * Creates a new comment
+	 * @param commentId unique identifier for each comment
+	 * @param postId post that the comment is being made on
+	 * @param author writer of the comment
+	 * @param comment body of the comment
+	 */
+	public Comment(int commentId, int postId, String author, String comment) {
+		super();
+		this.commentId = commentId;
+		this.postId = postId;
+		this.author = author;
+		this.comment = comment;
+	}
+
+	public Comment() {
+		super();
+	}
+
+	public int getCommentId() {
+		return commentId;
+	}
+
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+
+	public int getPostId() {
+		return postId;
+	}
+
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [commentId=" + commentId + ", postId=" + postId + ", author=" + author + ", comment=" + comment
+				+ "]";
+	}
+
+}
