43e6859a78341fec9e6a54a10e8901b445c8de77
Paul <benderp94@gmail.com>
2020-11-08 12:52:11 -0800

    Still working on the automated deployment

--- src/main/java/com/web/service/PostService.java
+++ src/main/java/com/web/service/PostService.java
@@ -4,10 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.web.dao.PostDao;
 import com.web.model.Post;
-
+@Service
 public class PostService {
 
 	private PostDao pd;
